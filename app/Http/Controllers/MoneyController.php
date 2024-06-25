0bc874403c10026f0b06fa4481822e20ec798827
NaNimimi <153357691+NaNimimi@users.noreply.github.com>
2024-06-25 15:09:19 +0500

    Update MoneyController.php

--- app/Http/Controllers/MoneyController.php
+++ app/Http/Controllers/MoneyController.php
@@ -1,5 +1,4 @@
 <?php
-
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
@@ -12,6 +11,23 @@ class MoneyController extends Controller
         try {
             $cashbox = Money::with(['customer', 'doctor', 'service'])->get();
             return response()->json($cashbox, 200);
+        } catch (\Exception $e) {
+            return response()->json(['error' => $e->getMessage()], 400);
+        }
+    }
+
+    public function inkassa()
+    {
+        try {
+            
+            $totalMoney = Money::sum('amount');
+
+            
+            if ($totalMoney != 0) {
+                return response()->json(['error' => 'Total money is not zero'], 400);
+            }
+
+            return response()->json(['message' => 'Total money is zero'], 200);
         } catch (\Exception $e) {
             return response()->json(['error' => $e->getMessage()], 400);
         }
